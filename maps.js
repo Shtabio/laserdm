3bdad695c34d9db6f26c942ac40520844750c3ce
Shtabio <shtabnoy96@gmail.com>
2026-07-29 14:12:21 +0300

    Update maps.js

--- maps.js
+++ maps.js
@@ -3,24 +3,24 @@ ymaps.ready(init);
 function init() {
 
     const map1 = new ymaps.Map("map1", {
-        center: [59.999, 30.233],
+        center: [60.030182, 30.206048],
         zoom: 16
     });
 
     map1.behaviors.enable("scrollZoom");
 
     map1.geoObjects.add(
-        new ymaps.Placemark([59.999, 30.233])
+        new ymaps.Placemark([60.030182, 30.206048])
     );
 
     const map2 = new ymaps.Map("map2", {
-        center: [59.970, 30.312],
+        center: [59.985233, 30.310603],
         zoom: 16
     });
 
     map2.behaviors.enable("scrollZoom");
 
     map2.geoObjects.add(
-        new ymaps.Placemark([59.970, 30.312])
+        new ymaps.Placemark([59.985233, 30.310603])
     );
 }
